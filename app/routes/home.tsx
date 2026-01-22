7c6a156e79f0374d35a94d814443369c33234ac0
swagsarthak <sarthakswain20500@gmail.com>
2026-01-21 19:17:59 -0500

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,43 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import Navbar from "~/components/Navbar";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Hive resume" },
+    { name: "description", content: "Personal ATS" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover" >
+    <Navbar />
+    {}
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>
+          Track Application rating
+        </h1>
+        <h2>
+          Review submissions & Check AI powered ratings
+        </h2>
+      </div>
+      {resumes.map((resume) =>(
+          <ResumeCard key={resume.id} resume={resume} />
+      ))}
+    </section>
+
+
+
+  </main>
 }
